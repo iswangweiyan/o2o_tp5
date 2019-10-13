a701edafd70a338319a42d749d7ec38f932b90c1
iswangweiyan <iswangweiyan@foxmail.com>
2019-10-13 19:12:58 +0800

    测试提交

--- imooc_o2o/public/router.php
+++ imooc_o2o/public/router.php
@@ -9,7 +9,7 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // $Id$
-
+//ss
 if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
     return false;
 } else {
